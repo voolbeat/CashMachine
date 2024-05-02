ccee359d578fd84f83bb4ed8cb2b9fe010c7207c
voolbeat <mishan-97@mail.ru>
2024-05-02 12:04:34 +0300

    The third step of project

--- CashMachine/Program.cs
+++ CashMachine/Program.cs
@@ -8,28 +8,39 @@ class Program
         Dictionary<int, int> b = new Dictionary<int, int> { { 500, 5 }, { 100, 10 }, { 50, 5 }, { 10, 5 } };
         PrintBanknotes(b);
         Console.Write("Введите сумму: ");
-        int s = Convert.ToInt32(Console.ReadLine()), r;
-        Dictionary<int, int> o = new Dictionary<int, int>();
-        foreach (KeyValuePair<int, int> p in b)
-        {
-            r = s / p.Key;
-            if (r > p.Value) r = p.Value;
-            s -= r * p.Key;
-            o.Add(p.Key, r);
-            if (s == 0) break;
-        }
-        if (s != 0)
+        int s = Convert.ToInt32(Console.ReadLine());
+        Dictionary<int, int> o = CalculateNotes(b, s);
+        if (o == null)
         {
             Console.WriteLine("Невозможно выдать сумму!");
             return;
         }
-        Console.WriteLine("Выданные купюры:");
-        foreach (KeyValuePair<int, int> p in o) Console.WriteLine(p.Key + ": " + p.Value);
+        PrintIssuedNotes(o);
     }
 
     static void PrintBanknotes(Dictionary<int, int> banknotes)
     {
         Console.WriteLine("Купюры:");
         foreach (KeyValuePair<int, int> p in banknotes) Console.WriteLine(p.Key + ": " + p.Value);
+    }
+
+    static Dictionary<int, int> CalculateNotes(Dictionary<int, int> banknotes, int amount)
+    {
+        Dictionary<int, int> issuedNotes = new Dictionary<int, int>();
+        foreach (KeyValuePair<int, int> p in banknotes)
+        {
+            int count = amount / p.Key;
+            if (count > p.Value) count = p.Value;
+            amount -= count * p.Key;
+            issuedNotes.Add(p.Key, count);
+            if (amount == 0) break;
+        }
+        return amount == 0 ? issuedNotes : null;
+    }
+
+    static void PrintIssuedNotes(Dictionary<int, int> issuedNotes)
+    {
+        Console.WriteLine("Выданные купюры:");
+        foreach (KeyValuePair<int, int> p in issuedNotes) Console.WriteLine(p.Key + ": " + p.Value);
     }
 }
